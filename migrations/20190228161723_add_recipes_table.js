2b32f45b2e31271e10effd54e93023c17bab8b97
samjeffcoat <samjeffcoat@gmail.com>
2019-02-28 16:42:38 -0600

    Adds recipes table

--- migrations/20190228161723_add_recipes_table.js
+++ migrations/20190228161723_add_recipes_table.js
@@ -1,8 +1,20 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('recipes', function(tbl){
+      tbl.increments();
+      tbl
+      .string('recipe_name', 128)
+      .notNullable()
+      .unique();
+
+      tbl
+      .integer('dish_id').unsigned()
+      tbl.foreign('dish_id').references('id').on('dishes')
+
+      tbl.timestamps(true, true);
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('recipes');
 };
